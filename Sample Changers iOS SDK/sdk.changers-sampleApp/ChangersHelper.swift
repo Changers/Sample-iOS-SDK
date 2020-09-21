ee6602be1184054da4268205e313249ad9564a62
Clément YEROCHEWSKI <yerochewski@gmail.com>
2020-09-21 14:03:17 +0200

    * update to latest Changers SDK

--- Sample Changers iOS SDK/sdk.changers-sampleApp/ChangersHelper.swift
+++ Sample Changers iOS SDK/sdk.changers-sampleApp/ChangersHelper.swift
@@ -15,7 +15,7 @@ extension ChangersEnv {
         case .development:
             return "client_secret"
         case .production:
-            return "client_secret"
+            return "w14mkOzaFvFKnWGc0vQmCyC6QJSXoeGV7bAPlDKD"
         case .stage:
             return "client_secret"
         }
@@ -26,14 +26,14 @@ extension ChangersEnv {
         case .development:
             return 0
         case .production:
-            return 0
+            return 2
         case .stage:
             return 0
         }
     }
     
     var clientName: String {
-        return  "client_name"
+        return  "darmstadt"
     }
 }
 
